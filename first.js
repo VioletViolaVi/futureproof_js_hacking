76a7a0e8a5a3905f744ea6f7c56d8265d57a96ab
vivian <vivian.v18723@gmail.com>
2022-07-08 15:57:59 +0100

    did fizzbuzz

--- first.js
+++ first.js
@@ -1,3 +1,19 @@
-for (let i = 0; i <= 10; i++) {
-  console.log(i);
+// Loop through the numbers from 1 to 100;
+// For each number:
+// If the number is evenly divisible by 3, print 'Fizz'
+// If the number is evenly divisible by 5, print 'Buzz'
+// If the number is divisible by 3 and 5, print 'FizzBuzz'
+// Otherwise, print the number;
+// 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, 10
+
+for (let i = 0; i < 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
 }
